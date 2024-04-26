7df321e5acf39dec12c7fab327f464a09be29456
dariocazzani <dariocazzani@gmail.com>
2024-04-26 12:35:12 -0400

    Ignore results file

--- bitnet/config.py
+++ bitnet/config.py
@@ -3,8 +3,8 @@ import os
 
 class ProjectConfig:
     EXAMPLES_DIR: str = "examples"
-    RESULTS_FILE: str = "experiment_results.json"
     PAPER_DIR: str = "paper"
+    RESULTS_FILE: str = os.path.join(PAPER_DIR, "experiment_results.json")
     PAPER_TEX_PATH: str = os.path.join(f"{PAPER_DIR}", "main.tex")
 
 
